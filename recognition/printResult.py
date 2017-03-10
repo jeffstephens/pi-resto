8d10d084858212a23f4c1f76276a3d7d4a5d0622
Jeffrey Stephens <jeffrey.stephens@readytalk.com>
2017-03-10 14:18:38 -0700

    Script to print song ID result to stdout

--- recognition/printResult.py
+++ recognition/printResult.py
@@ -1,6 +1,13 @@
 import sys, json
 
-resultJson=json.loads(sys.stdin.read())
+resultJson = json.loads(sys.stdin.read())
 
-#if resultJson["status"]["msg"] != "Success"
+# exit immediately if recognition failed
+if resultJson["status"]["msg"] != "Success":
+	print "Recognition failed."
+	sys.exit(2)
 
+songName = resultJson["metadata"]["music"][0]["title"]
+songArtist = resultJson["metadata"]["music"][0]["artists"][0]["name"]
+
+print "Heard '%s' by %s" % (songName, songArtist)
