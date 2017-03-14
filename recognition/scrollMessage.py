834afceed7fc4183ca3d7f60e763823a2ef7cca1
Jeff Stephens <jeffrey.stephens@readytalk.com>
2017-03-14 18:34:56 +0000

    Slow down scroll speed for demo

--- recognition/scrollMessage.py
+++ recognition/scrollMessage.py
@@ -2,4 +2,4 @@ from sense_hat import SenseHat
 import sys
 
 sense = SenseHat()
-sense.show_message(sys.stdin.read(), scroll_speed=.05, text_colour=[128, 0, 128])
+sense.show_message(sys.stdin.read(), scroll_speed=.08, text_colour=[128, 0, 128])
